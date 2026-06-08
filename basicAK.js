36ed8479f9bd145dfabdaf16bcd9c95a685220a2
sudhantech <sdhrsanm@gmail.com>
2026-06-08 18:31:29 +0530

    if else exercise

---
+++ basicAK.js
@@ -0,0 +1,15 @@
+var score=78
+if(score < 50)
+{
+  console.log("You need to improve")
+}
+else if(score > 50 && score <70)
+{
+  console.log("Good job!")
+}
+else if(score >= 70){
+  console.log("Excellent performance!")
+}
+else{
+  console.log("very bad!")
+}
