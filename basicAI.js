64c5a222166944d6bafce0fb5d053b65dfc18147
sudhantech <sdhrsanm@gmail.com>
2026-06-08 16:44:01 +0530

    logical operator & | =!

---
+++ basicAI.js
@@ -0,0 +1,13 @@
+// logical and or not opperator used to write the code
+var traffic_light="red"
+if(traffic_light)
+{
+  console.log("stop")
+}
+else if(traffic_light)
+{
+  console.log("Get ready")
+}
+else{
+  console.log("Go.")
+}
