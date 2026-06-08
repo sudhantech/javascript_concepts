cc488a0ba4b1bc8b8f58bb6c8fe433ec3cb8c74b
sudhantech <sdhrsanm@gmail.com>
2026-06-08 17:00:10 +0530

    if elseexercise

---
+++ basicAJ.js
@@ -0,0 +1,14 @@
+var year= "spring"
+if(year == "spring"){
+  console.log("Enjoy the blooming flowers")
+}
+
+else if(year == "summer"){
+  console.log("Have fun in the sun")
+}
+else if(year =="autumn" || year == "fall"){
+  console.log("Admire the colorful leaves.")
+}
+else if(year == "winter"){
+  console.log("Bundle up and stay warm.")
+}
