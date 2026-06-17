6cf39e1abf3ddcde8945619433d6f23a776a938f
sudhantech <sdhrsanm@gmail.com>
2026-06-17 19:57:04 +0530

    create the multiple var then call the variables

---
+++ basicAN.js
@@ -0,0 +1,7 @@
+var factor="Vijai"
+var fplayer="Donhi"
+var fMovie="jana naayagan"
+
+console.log("Your favourit actor :",factor);
+console.log("Your favourit player :",fplayer);
+console.log("Your favourit movie :",fMovie);
